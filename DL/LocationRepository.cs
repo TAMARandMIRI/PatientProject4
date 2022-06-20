c6eb72f24d546033bdf1bb23e9a00eba8ae4c79b
MIRI <Miriklainer@gmail.com>
2022-06-20 15:20:43 +0300

    c

--- DL/LocationRepository.cs
+++ DL/LocationRepository.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;  
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
